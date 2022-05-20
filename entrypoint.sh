35066bcad3bda046d56647ac4043deec3d25150d
Dan Snow <snow.dfs@gmail.com>
2022-05-20 17:39:36 +0000

    Swap Docker backend to use Athena

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # Launch shinyproxy with the trusted keystore containing CCAO public cert
-java \
-  -Djavax.net.ssl.trustStore=/run/secrets/DC_CERT \
-  -Djavax.net.ssl.trustStorePassword=${JAVA_TRUST_STORE_PASSWORD} \
-  -jar ${WORKDIR}/shinyproxy.jar
+java -jar ${WORKDIR}/shinyproxy.jar
